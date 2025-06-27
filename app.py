96b49b3bc0fb260c64a2dc9de63756283c4ec9c2
KingsData-Web <o.mather@warwickschools.co.uk>
2025-06-27 12:44:52 +0100

    Update app.py

--- app.py
+++ app.py
@@ -4,8 +4,5 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def index():
-    return render_template('index.html')
-
-if __name__ == '__main__':
-    app.run(debug=True)
+def home():
+    return render_template('index.html')
